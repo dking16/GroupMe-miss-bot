1815c1117b8645c5e63fa1e598c40d4044a16efd
Declan King <daking@mit.edu>
2025-05-19 18:31:18 -0400

    different port

--- app.py
+++ app.py
@@ -54,5 +54,5 @@ def recieve_message():
         return jsonify({"status": "ignored", "reason": "not target user"}), 200
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80, debug=True) # Added debug=True for development
+    app.run(host='0.0.0.0', port=8789, debug=True) # Added debug=True for development
     # app.run(debug=True)
